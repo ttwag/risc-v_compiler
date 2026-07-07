711f9dcc4688bb8322a6c5ae16c82bde61a8aa1c
ttwag <taowang@ucdavis.edu>
2026-07-07 14:44:53 +0800

    [TEST] Add integration test for main.rs
    - WHAT?
        1. Add integration test for main that tests if files could be compiled into assembly from command line commands to actual file construction
    - HOW?
        1. Add files (marked with `ok_` and `err_`) into a new `tests/fixtures` directory and integration test calls the compiled compiler to compile them and assert compilation success or failure based on `ok_` or `err_`
    - WHY?
        1. Test if the compiler could be called and used from the command line

---
+++ tests/main_test.rs
@@ -0,0 +1,42 @@
+use std::fs;
+use std::path::Path;
+use std::process::Command;
+use tempfile::tempdir;
+
+fn assert_compile(path: &Path) {
+    for entry in fs::read_dir(&path).unwrap() {
+        let path = entry.unwrap().path();
+        let name = path.file_stem().unwrap().to_str().unwrap();
+        let should_succeed = name.starts_with("ok_");
+
+        let output = Command::new(env!("CARGO_BIN_EXE_compile"))
+            .arg(&path)
+            .output()
+            .expect("compiler crashed");
+
+        assert_eq!(
+            output.status.success(),
+            should_succeed,
+            "[{name}] unexpected result: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+}
+
+#[test]
+fn test_main_compile() {
+    let temp_workspace = tempdir().unwrap();
+    let temp_path = temp_workspace.path();
+    let fixture_src = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures");
+    
+    // copy all files from test fixtures to tempdir
+    Command::new("cp")
+        .arg("-r")
+        .arg(fixture_src.join("."))
+        .arg(temp_path)
+        .status()
+        .expect("failed to copy to a temporary test directory");
+
+    // assert the files compile and return with the correct statuses
+    assert_compile(temp_path);
+}
